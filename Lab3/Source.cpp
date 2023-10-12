cd6e1a971365fc35c35f7f213ad50c1f3027854a
Palmaliv <mr.player.203@mail.ru>
2023-10-12 22:30:27 +0400

    Update

--- Lab3/Source.cpp
+++ Lab3/Source.cpp
@@ -13,56 +13,79 @@
 #include <vector>
 #include <iomanip>
 
+void drawPascalTriangle(int n);
+void drawLine(std::vector<int> line, int i, int m);
+void test();
+
 
 int main() {
 	setlocale(LC_ALL, "Rus");
 
-	int n;
+	/*int n;
 	std::cout << "Введите количество строк треугольника Паскаля: ";
 	std::cin >> n;
 
-	int m = n / 2; // Количество элементов в полустроке
+	drawPascalTriangle(n); */
+
+	test();
+}
+
+
+void drawPascalTriangle(int n) {
+	if (n <= 1)
+		return drawLine({ 1 }, 1, 1);
+
+	int m = n / 2 + n % 2; // Количество элементов в полустроке
 	std::vector<int> line(m);
 	line[0] = 1; // Инициализация первой строки
 
-	int temp1, temp2; // Переменные для хранение
-	int count = 1; // Количесвто элементов для расчёта
+	drawLine(line, 1, m);
+
 	for (size_t i = 1; i < n; i++) {
-
-		// Вывод левой части строки
-		for (size_t j = 0; j < m; j++) {
-			if (line[m - j - 1] == 0)
-				std::cout << "      ";
-			else
-				std::cout << std::setw(6) << line[m - j - 1];
-		}
-
-		// Вывод правой части строки
-		for (size_t j = (i % 2); j < m; j++) {
-			if (line[j] == 0)
-				std::cout << "      ";
-			else
-				std::cout << std::setw(6) << line[j];
-		}
-
-		std::cout << std::endl;
-
-		count += !(i % 2);
-
 		// Проверка на симметричность/чётность строки
 		if (i % 2) {
-			for (size_t j = 0; j < count - 1; j++)
+			for (size_t j = 0; j < m - 1; j++)
 				line[j] += line[j + 1];
 		}
 		else {
+			int temp1, temp2; // Переменные для хранение
 			temp1 = line[0];
 			line[0] *= 2;
 
-			for (size_t j = 1; j < count; j++) {
+			for (size_t j = 1; j < m; j++) {
 				temp2 = line[j];
 				line[j] = temp1 + line[j];
 				temp1 = temp2;
 			}
 		}
+
+		drawLine(line, i + 1, m);
+	}
+}
+
+void drawLine(std::vector<int> line, int i, int m) {
+	std::cout << std::setw(3) << i << " |";
+
+	// Вывод левой части строки
+	for (size_t j = 0; j < m; j++) {
+		if (line[m - j - 1] != 0)
+			std::cout << std::setw(6) << line[m - j - 1];
+	}
+
+	// Вывод правой части строки
+	for (size_t j = (i % 2); j < m; j++) {
+		if (line[j] != 0)
+			std::cout << std::setw(6) << line[j];
+	}
+
+	std::cout << std::endl;
+}
+
+
+void test() {
+	for (size_t i = 0; i < 20; i++) {
+		drawPascalTriangle(i + 1);
+
+		std::cout << std::endl;
 	}
 }
